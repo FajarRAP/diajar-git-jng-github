84d6962162e0eb5b1de479e5a512426d4f2982e5
TititUpin <fajary781@gmail.com>
2022-03-23 16:13:00 +0700

    Nambah kata siku-siku jng nambah cpp nama nim

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int main(){
+	cout<<"Segitiga siku-siku : "<<endl;
 	for(int a=0; a<6; a++){
 		for(int b=0; b<a; b++){
 			cout<<"*";
---
+++ Tambahan.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout<<"Fajar Riansyah"<<endl;
+	cout<<"  2100018165"<<endl;
+	return 0;
+}
