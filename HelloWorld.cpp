7b0a2710715d19c90e1aad98cb5731594bf1ff79
FajarRAP <fajary781@gmail.com>
2022-03-23 20:57:01 +0700

    menambah while loop input kata

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,12 +2,21 @@
 using namespace std;
 
 int main(){
+	string kata;
 	cout<<"Segitiga siku-siku : "<<endl;
 	for(int a=0; a<6; a++){
 		for(int b=0; b<a; b++){
 			cout<<"*";
 		}
 		cout<<endl;
+	}
+
+	while(true){
+		cout<<"Masukkan karakter : ";
+		cin>>kata;
+		if(kata=='q'){
+			break;
+		}
 	}
 	return 0;
 }
